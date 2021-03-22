c406ee7dc6e8b4e1ba0eb5e493a603aabf62ece1
Daniel Saromo Mori <danielsaromo@gmail.com>
2021-03-22 00:39:32 -0500

    Update keras_test.py

--- tests/keras_test.py
+++ tests/keras_test.py
@@ -14,7 +14,7 @@ from tensorflow.keras.layers import Dense
 from ARPkeras import AutoRotDense
 
 model = Sequential()
-model.add(Dense(20), input_shape=(123,))
+model.add(Dense(20, input_shape=(123,)))
 model.add(AutoRotDense(10, 0, 1, 4, activation='sigmoid'))
 
 model.summary()
